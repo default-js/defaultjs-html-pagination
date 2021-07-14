5bb04e850171414d01e472462f8a8fb50d376616
Frank Schüler <schuelerfrank@gmx.de>
2021-07-14 10:02:56 +0200

    release 1.0.3 commit all other files

---
+++ dist/index.2549647540.js
@@ -0,0 +1,9 @@
+(self["webpackChunk_default_js_defaultjs_html_pagination"] = self["webpackChunk_default_js_defaultjs_html_pagination"] || []).push([["index.2549647540"],{},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["commons"], () => (__webpack_exec__("./test/index.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
